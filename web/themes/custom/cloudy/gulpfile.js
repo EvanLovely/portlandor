4569119724cf0819b8197bf58f48526fa62ed2a6
brittanysmart <brittany.smart@portlandoregon.gov>
2018-10-30 12:52:14 -0700

    POWR-522-1 gulp pa11y

--- web/themes/custom/cloudy/gulpfile.js
+++ web/themes/custom/cloudy/gulpfile.js
@@ -2,7 +2,8 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
-var axe = require('gulp-axe-webdriver');
+var pa11y = require('gulp-pa11y');
+var runSequence = require('run-sequence');
 var concat = require("gulp-concat");
 var minifyCss = require("gulp-minify-css");
 var uglify = require("gulp-uglify");
@@ -27,14 +28,12 @@ gulp.task('js', function() {
         .pipe(browserSync.stream());
 });
 
-gulp.task('axe', function(done) {
-  var options = {
-    saveOutputIn: 'allHtml.json',
-    browser: 'firefox',
-    urls: ['https://alpha.portland.gov']
-  };
-  return axe(options, done);
-});
+gulp.task('pa11y', pa11y({
+  url: 'https://portlandor.lndo.site/',
+  failOnError: true, // fail tje build on error
+  showFailedOnly: true, // show errors only and override reporter
+  reporter: 'console'
+}));
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['style'], function() {
@@ -46,5 +45,7 @@ gulp.task('serve', ['style'], function() {
     gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'scss/**/*.scss'], ['style']);
     //    gulp.watch("src/*.html").on('change', browserSync.reload);
 });
+
+gulp.task('build', function(callback) { runSequence('pa11y', callback); });
 
 gulp.task('default', ['js', 'serve']);
