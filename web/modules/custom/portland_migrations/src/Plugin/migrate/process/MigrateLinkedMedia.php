9d4672c2d737b41503a3d3129622d8e50599ea06
Greg Clapp <gregory.clapp@portlandoregon.gov>
2019-07-31 10:33:42 -0700

    POWR-1102 custom process plugin for migrating linked documents in body content

--- web/modules/custom/portland_migrations/src/Plugin/migrate/process/MigrateLinkedMedia.php
+++ web/modules/custom/portland_migrations/src/Plugin/migrate/process/MigrateLinkedMedia.php
@@ -11,9 +11,13 @@ use Drupal\Component\Utility\Xss;
 use Drupal\media\Entity\Media;
 
 /**
+<<<<<<< HEAD
  * The MigrateLinkedMedia plugin parses content and looks for images and links to documents.
  * If found, the source URL is used to download and store the file as a media entity, and the
  * link or image tag is converted to an entity-embed element.
+=======
+ * Description of what the plugin does
+>>>>>>> POWR-1102 custom process plugin for migrating linked documents in body content
  *
  * @MigrateProcessPlugin(
  *   id = "migrate_linked_media"
@@ -25,11 +29,23 @@ class MigrateLinkedMedia extends ProcessPluginBase {
    */
   public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
 
+<<<<<<< HEAD
     preg_match_all('/<a [^>]+>|<img [^>]+>/i', $value, $result);
+=======
+    // when run against a HTML content field during migration, the following things happen:
+
+    // * Look for all internal links (either start with "/" or "http*://portlandoregon.gov")
+    // ** Request target; check mime-type
+    // ** If HTML, try to use path for lookup if page exists in new system; create a basic link
+    // ** If document, create a media entity and replace the link tag with media-embed tag in body content
+
+    preg_match_all('/<a [^>]+>/i', $value, $result);
+>>>>>>> POWR-1102 custom process plugin for migrating linked documents in body content
     if (!empty($result[0])) {
       // load content into xpath dom so we can search it more easily
       $dom = Html::load($value);
       $xpath = new \DOMXPath($dom);
+<<<<<<< HEAD
       // do we need to deal with case sensitivity?
       // $xpath->registerNamespacce('php', 'http://php.net/xpath');
       // $xpath->registerPhpFunctions();
@@ -236,10 +252,120 @@ class MigrateLinkedMedia extends ProcessPluginBase {
             'langcode' => \Drupal::languageManager()->getDefaultLanguage()->getId(),
             'name' => $link_text,
             'status' => 1,
+=======
+
+      // determine download folder name (YYYY-MM)
+      $folder = date("Y-m") ;
+      $public_path = \Drupal::service('file_system')->realpath(file_default_scheme() . "://");
+      $download_path = $public_path . "/" . $folder;
+      $result = file_prepare_directory($download_path, FILE_CREATE_DIRECTORY);
+
+      // look for links with an href
+      foreach ($xpath->query('//a[@href]') as $link) {
+
+        $link_text = $link->textContent;
+        if (function_exists('transliterate_filenames_transliteration')) {
+          $filename = transliterate_filenames_transliteration($link_text, $source_langcode);
+        }
+
+        // parse url from link
+        $url = $link->getAttribute('href'); // /shared/cfm/image.cfm?id=25873
+        if (substr(0, 26) != "https://portlandoregon.gov" && substr(0,125) != "https://www.portlandoregon.gov") {
+          $url = "https://www.portlandoregon.gov" . $url;
+        }
+
+        // need uri for managed files
+        $uri = file_build_uri($folder);
+
+        // download and store file
+        $orig_filename = pathinfo(parse_url($url, PHP_URL_PATH), PATHINFO_FILENAME);
+        if (!file_exists($uri . '/' . $orig_filename)) {
+          $result = system_retrieve_file($url, $uri, TRUE);
+        } else {
+          $files = \Drupal::entityTypeManager()->getStorage('file')->loadByProperties(['uri' => $uri]);
+          $result = $files;
+        }
+        if (!$result) continue;
+        
+        $absolute_path = \Drupal::service("file_system")->realpath($uri . "/" . $result->filename->value);
+
+        // get mime type
+        $mime_type = mime_content_type($absolute_path); // application/pdf
+
+        // determine extension from mime type
+        switch ($mime_type) {
+          case "application/pdf":
+            $ext = "pdf";
+            $type = "document";
+            break;
+          case "application/msword":
+            $ext = "doc"; 
+            $type = "document";
+            break;
+          case "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+            $ext = "docx"; 
+            $type = "document";
+            break;
+          case "application/vnd.ms-excel":
+            $ext = "xls"; 
+            $type = "document";
+            break;
+          case "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
+            $ext = "xlsx"; 
+            $type = "document";
+            break;
+          case "application/vnd.ms-powerpoint":
+            $ext = "ppt"; 
+            $type = "document";
+            break;
+          case "application/vnd.openxmlformats-officedocument.presentationml.presentation":
+            $ext = "pptx"; 
+            $type = "document";
+            break;
+          case "image/jpeg":
+            $ext = "jpg"; 
+            $type = "image";
+            break;
+          case "image/gif":
+            $ext = "gif"; 
+            $type = "image";
+            break;
+          case "image/png":
+            $ext = "png"; 
+            $type = "image";
+            break;
+          case "text/html":
+            // it's a link to a page, delete it instead of naming it
+            $result->delete();
+            continue 2;
+          default:
+            // other, log it
+            $message = "Unhandled download encountered in migration: " . $mime_type . "<br>File path: " . $absolute_path . "<br>From URL: " . $url;
+            \Drupal::logger('portland_migrations')->notice($message);
+            continue 2;
+        }
+
+        // rename file if it doesn't already exist.
+        // TODO: if it does exist, delete the temp file and don't rename.
+        $file = \Drupal\file\Entity\File::load($result->fid->value);
+        $new_filename = $folder . "/" . $filename . "." . $ext;
+        $stream_wrapper = \Drupal::service('file_system')->uriScheme($file->getFileUri());
+        $new_filename_uri = "{$stream_wrapper}://{$new_filename}";
+        file_move($file, $new_filename_uri);
+
+        // create media object for file (image or document)
+        if ($type == "document") {
+          $media = Media::create([
+            'bundle' => 'document',
+            'uid' => \Drupal::currentUser()->id(),
+            'langcode' => \Drupal::languageManager()->getDefaultLanguage()->getId(),
+            'name' => $link_text,
+>>>>>>> POWR-1102 custom process plugin for migrating linked documents in body content
             'field_document' => [
               'target_id' => $file->id()
             ],
           ]);
+<<<<<<< HEAD
         } else if ($file_info[1] == "image") {
           $media = Media::create([
             'bundle' => 'image',
@@ -258,18 +384,33 @@ class MigrateLinkedMedia extends ProcessPluginBase {
         $uuid = $media->uuid();
 
         // replace <a> or <img> with <entity-embed>
+=======
+          $media->setPublished();
+          $media->save();
+        }
+        $uuid = $media->uuid();
+
+        // replace link with entity-embed tag
+
+>>>>>>> POWR-1102 custom process plugin for migrating linked documents in body content
         $node = $dom->createElement("drupal-entity");
         $newnode = $dom->appendChild($node);
         $newnode->setAttribute("data-entity-type", "media");
         $newnode->setAttribute("data-entity-uuid", $uuid);
         $newnode->setAttribute("data-langcode", "en");
+<<<<<<< HEAD
         if ($file_info[1] == "document") {
+=======
+
+        if ($type == "document") {
+>>>>>>> POWR-1102 custom process plugin for migrating linked documents in body content
           $newnode->setAttribute("data-embed-button", "document_browser");
           $newnode->setAttribute("data-entity-embed-display", "view_mode:media.embedded");
         } else {
           $newnode->setAttribute("data-embed-button", "image_browser");
           $newnode->setAttribute("data-entity-embed-display", "media_image");
         }
+<<<<<<< HEAD
         $link->parentNode->replaceChild($newnode, $link);
       }
       $output = Html::serialize($dom);
@@ -279,4 +420,18 @@ class MigrateLinkedMedia extends ProcessPluginBase {
     return $value;
   }
 
+=======
+
+        $link->parentNode->replaceChild($newnode, $link);
+      }
+    }
+  }
+
+  function portland_migrations_parseUrl($link) {
+    return $url;
+  }
+
+  function portland_migrations_getFileType($url) {
+  }
+>>>>>>> POWR-1102 custom process plugin for migrating linked documents in body content
 }
