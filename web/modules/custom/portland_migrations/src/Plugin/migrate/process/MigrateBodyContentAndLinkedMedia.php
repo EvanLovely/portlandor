250f142745239ec047f0a530f6e324af94c86ebc
Greg Clapp <gregory.clapp@portlandoregon.gov>
2019-08-15 12:26:01 -0700

    POWR-1102 fixed issues with entity-embed tag

--- web/modules/custom/portland_migrations/src/Plugin/migrate/process/MigrateBodyContentAndLinkedMedia.php
+++ web/modules/custom/portland_migrations/src/Plugin/migrate/process/MigrateBodyContentAndLinkedMedia.php
@@ -83,144 +83,7 @@ class MigrateBodyContentAndLinkedMedia extends ProcessPluginBase {
           }
         }
 
-        // if ($downloaded_file === false) {
-        //   // the file could not be retrieved from the file system, which indicates it
-        //   // doesn't exist. the most likely reason is that the url was a 404.
-        //   $message = "Error occurred while trying to open file saved from " . $url . ". Most likely reason is a 404 error.";
-        //   \Drupal::logger('portland_migrations')->notice($message);
-        //   continue;
-        // }
-
-        // NOTE: The following processing is unnecessary since we can now get the original filename
-        // and exclude non-binary files.
-
-        // process the file: 
-        // delete it if it's not a type we want, or rename it with a more descriptive filename
-        // and appropriate extension. when we saved the raw file from POG, it was saved with
-        // whatever name was in the URL (typically the content id).
-
-        // try {
-        //   $absolute_path = \Drupal::service("file_system")->realpath($destination_uri);
-        //   $mime_type = mime_content_type($absolute_path);
-        // }
-        // catch (Exception $e) {
-        //   $message = "Problem getting mime type for file (possible 404) " . $file_url . " at " . $absolute_path;
-        //   \Drupal::logger('portland_migrations')->notice($message);
-        // }
-
-        // switch ($mime_type) {
-        //   case "application/pdf":
-        //     $file_info = ["pdf", "document"]; break;
-        //   case "application/msword":
-        //     $file_info = ["doc", "document"]; break;
-        //   case "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
-        //     $file_info = ["docx", "document"]; break;
-        //   case "application/vnd.ms-excel":
-        //     $file_info = ["xls", "document"]; break;
-        //   case "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
-        //     $file_info = ["xlsx", "document"]; break;
-        //   case "application/vnd.ms-powerpoint":
-        //     $file_info = ["ppt", "document"]; break;
-        //   case "application/vnd.openxmlformats-officedocument.presentationml.presentation":
-        //     $file_info = ["pptx", "document"]; break;
-        //   case "image/jpeg":
-        //     $file_info = ["jpg", "image"]; break;
-        //   case "image/gif":
-        //     $file_info = ["gif", "image"]; break;
-        //   case "image/png":
-        //     $file_info = ["png", "image"]; break;
-        //   case "text/html":
-        //     $file_info = ["html", null]; break;
-        //   default:
-        //     $file_info = null; break;
-        // }
-
-        // TODO: How are we handling links to HTML pages in legacy site? Keep legacy URL?
-        // // TODO: if url is relative and mime type is html, do we need to update the url to be from POG, or just keep it relative?
-        // if ($file_info[0] == "html" && $internal_link) {
-        //   $link->setAttribute("href", $url);
-        // }
-
-        // // if ext is html, delete the downloaded file and continue the loop. We'll leave the link as it is.
-        // if ($file_info[0] == "html") {
-        //   if (isset($downloaded_file) && $downloaded_file) {
-        //     $downloaded_file->delete();
-        //   }
-        //   continue;
-        // }
-
-        // // if ext is a type we have not defined, log it, delete the file if it exists, and continue the loop.
-        // if (is_null($file_info)) {
-        //   if (!isset($mime_type) || !$mime_type || $mime_type == "") {
-        //     $mime_type = "Undefined mime type";
-        //   }
-        //   $message = "Undefined download encountered in migration: " . $mime_type . "<br>File path: " . $absolute_path . "<br>From URL: " . $url;
-        //   \Drupal::logger('portland_migrations')->notice($message);
-        //   if (isset($downloaded_file) && $downloaded_file) {
-        //     $downloaded_file->delete();
-        //   }
-        //   continue;
-        // }
-
-        // // use link text as file name.
-        // $link_text = $link->textContent;
-        // $filename = $link_text;
-        // if (function_exists('transliterate_filenames_transliteration')) {
-        //   $filename = transliterate_filenames_transliteration($link_text, $source_langcode);
-        // }
-
-        // // don't let filenames start with "link-" or "link-to-"
-        // if (substr($filename, 0, 8) == "link-to-") {
-        //   $filename = substr($filename, 8, strlen($filename));
-        // }
-        // if (substr($filename, 0, 5) == "link-") {
-        //   $filename = substr($filename, 5, strlen($filename));
-        // }
-
-        // TODO: It's possible that we could have two different files with the same name, since
-        // we're naming them based on the link text. There's no good way to determine the file
-        // name from the URL since most if the time it's just the content id. How big of a problem
-        // is this? Perhaps we should use incrememtal numbering in a test and reivew the duplicated
-        // files.
-
-        // // rename file if renamed file doesn't alredy exist. append content id on end of filename
-        // // to help differentiate between different files that might have the same link text.
         $file = \Drupal\file\Entity\File::load($downloaded_file->fid->value);
-        // $new_filename = $folder_name . "/" . $filename . $content_id . "." . $file_info[0];
-        // $stream_wrapper = \Drupal::service('file_system')->uriScheme($file->getFileUri());
-        // $new_filename_uri = "{$stream_wrapper}://{$new_filename}";
-        
-        // if (!file_exists($new_filename_uri)) {
-        //   $move_result = file_move($file, $new_filename_uri);
-        // } else {
-        //   // it exists, append to the filename to make it unique
-        //   // first see if appended filename exists
-        //   $exists = true;
-        //   $try_filename = $new_filename_uri;
-        //   $safety = 0;
-        //   while ($exists == true) {
-        //     $try_filename = $try_filename . "-copy";
-        //     if (!file_exists($try_filename)) {
-        //       // if appended filename doesn't exist, create it
-        //       $move_result = file_move($file, $try_filename);
-        //       $exists = false;
-        //     }
-        //     $safety = $safety + 1;
-        //     if ($safety > 25) {
-        //       $exists = false;
-        //     }
-          // }
-
-        //   // renamed file already exists. delete temp file, load existing renamed file, and 
-        //   // reference that in the embed tag.
-        //   $file->delete();
-        //   $existing_files = \Drupal::entityTypeManager()->getStorage('file')->loadByProperties(['uri' => $folder_uri . '/' . $filename . $content_id . '.' . $file_info[0]]);
-        //   $file = reset($existing_files); // should only ever be 1 file returned
-        // }
-
-        // if ($file == false) {
-        //   $halt = true;
-        // }
 
         // create media entity for file (image or document)
         if ($media_type == "document") {
@@ -252,18 +115,9 @@ class MigrateBodyContentAndLinkedMedia extends ProcessPluginBase {
         $uuid = $media->uuid();
 
         // replace <a> or <img> with <entity-embed>
-        $node = $dom->createElement("drupal-entity");
-        $newnode = $dom->appendChild($node);
-        $newnode->setAttribute("data-entity-type", "media");
-        $newnode->setAttribute("data-entity-uuid", $uuid);
-        $newnode->setAttribute("data-langcode", "en");
-        if ($file_info[1] == "document") {
-          $newnode->setAttribute("data-embed-button", "document_browser");
-          $newnode->setAttribute("data-entity-embed-display", "view_mode:media.embedded");
-        } else {
-          $newnode->setAttribute("data-embed-button", "image_browser");
-          $newnode->setAttribute("data-entity-embed-display", "media_image");
-        }
+        $embed_tag_node = $dom->createElement("drupal-entity");
+        $newnode = $dom->appendChild($embed_tag_node);
+        $newnode = $this->buildEmbedTagNode($newnode, $embed_tag_node, $uuid, $media_type);
         $link->parentNode->replaceChild($newnode, $link);
       }
       $output = Html::serialize($dom);
@@ -352,11 +206,18 @@ class MigrateBodyContentAndLinkedMedia extends ProcessPluginBase {
     }
   }
 
-  protected function getPogFileName($url) {
-    return $filename;
-  }
-
-  protected function testme() {
-    return "tested!";
+  protected function buildEmbedTagNode($newnode, $embed_tag_node, $uuid, $media_type) {
+    $newnode->setAttribute("data-entity-type", "media");
+    $newnode->setAttribute("data-entity-uuid", $uuid);
+    $newnode->setAttribute("data-langcode", "en");
+    if ($media_type == "document") {
+      $newnode->setAttribute("data-embed-button", "document_browser");
+      $newnode->setAttribute("data-entity-embed-display", "view_mode:media.embedded");
+    } else {
+      $newnode->setAttribute("data-embed-button", "image_browser");
+      $newnode->setAttribute("data-entity-embed-display", "media_image");
+      $newndoe->setAttribute("data-align", "right");
+    }
+    return $newnode;
   }
 }
