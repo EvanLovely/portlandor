b5a679b75f36f043d8d13fa9f31942365c3b2557
Greg Clapp <gregory.clapp@portlandoregon.gov>
2019-08-19 11:28:19 -0700

    POWR-1285 updated content for policy number migration.

--- web/modules/custom/portland_migrations/src/Plugin/migrate/process/MigratePolicyNumber.php
+++ web/modules/custom/portland_migrations/src/Plugin/migrate/process/MigratePolicyNumber.php
@@ -14,6 +14,10 @@ use Drupal\media\Entity\Media;
  * In the source data, the full policy number is in a 3-part format such as BCP-BHR-16.03.
  * The first segment is the policy type. The second and third segments comprise the policy
  * number that should be stored in field_policy_number.
+ * 
+ * The policies.csv datafile has been modified to incluee a POLICY_NUMBER column. It was
+ * previously stored in SUMMARY_TEXT on POG, but that column has been manually split to
+ * only include summary text. All policy number data has been moved to the new column.
  *
  * @MigrateProcessPlugin(
  *   id = "migrate_policy_number"
